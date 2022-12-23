13489ed4c1a99c5270cb293142332cc491ed7089
Chris Wilson <awil17@outlook.com>
2022-12-23 04:56:18 -0500

    Addeding Excerise 1 & 2

---
+++ Exercise1_02/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hi! This is my first program, what is your name?: ");
+
+var name = Console.ReadLine();
+
+Console.WriteLine($"Hello {name}, it is very nice to meet you. We have a fun journey ahead.");
